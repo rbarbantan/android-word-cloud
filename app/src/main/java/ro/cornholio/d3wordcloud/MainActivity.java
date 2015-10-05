cfe998787463808178cab5bf3c11b5192dddb957
Rares Barbantan <rares.barbantan@gmail.com>
2015-10-05 22:12:35 +0300

    initialize word cloud from Android isntead of JavaScript

--- app/src/main/java/ro/cornholio/d3wordcloud/MainActivity.java
+++ app/src/main/java/ro/cornholio/d3wordcloud/MainActivity.java
@@ -1,23 +1,51 @@
 package ro.cornholio.d3wordcloud;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] wordCloud = new String[]{ "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb",
+            "Ice Cream Sandwich", "Jelly Bean", "KitKat", "Lollipop", "Marshmallow"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView d3 = (WebView) findViewById(R.id.d3);
+        final WebView d3 = (WebView) findViewById(R.id.d3);
 
         WebSettings ws = d3.getSettings();
         ws.setJavaScriptEnabled(true);
         d3.loadUrl("file:///android_asset/d3.html");
+        d3.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                StringBuffer sb = new StringBuffer();
+                sb.append("wordCloud([");
+                for (int i = 0; i < wordCloud.length; i++) {
+                    sb.append("'").append(wordCloud[i]).append("'");
+                    if (i < wordCloud.length - 1) {
+                        sb.append(",");
+                    }
+                }
+                sb.append("])");
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    d3.evaluateJavascript(sb.toString(), null);
+                } else {
+                    d3.loadUrl("javascript:" + sb.toString());
+                }
+            }
+        });
     }
 
 }
